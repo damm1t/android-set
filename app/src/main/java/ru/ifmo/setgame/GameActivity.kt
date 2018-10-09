78809cfd3253dd9cbde347eb1d382ce44abab15b
Sergei <kozelkos@yandex.ru>
2018-10-09 18:38:35 +0300

    Add clickListener stub

--- app/src/main/java/ru/ifmo/setgame/GameActivity.kt
+++ app/src/main/java/ru/ifmo/setgame/GameActivity.kt
@@ -1,5 +1,6 @@
 package ru.ifmo.setgame
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayout
@@ -9,9 +10,10 @@ import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
 
-    val DEFAULT_COLUMNS = 3
-    val DEFAULT_ROWS = 4
+    private val DEFAULT_COLUMNS = 3
+    private val DEFAULT_ROWS = 4
 
+    private val board_state = MutableList(DEFAULT_COLUMNS * DEFAULT_ROWS){false}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +29,12 @@ class GameActivity : AppCompatActivity() {
                 val tv = TextView(this)
                 tv.text = "$i x $j"
                 tv.gravity = Gravity.CENTER
+
+                tv.setOnClickListener {
+                    val index = i * DEFAULT_COLUMNS + j
+                    board_state[index] = !board_state[index]
+                    it.setBackgroundColor(if(board_state[index]) Color.RED else Color.WHITE)
+                }
 
                 game_grid.addView(tv, params)
             }
