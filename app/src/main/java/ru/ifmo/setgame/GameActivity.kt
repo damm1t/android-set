3c6667fba32481fcfba455272c431d55a5d1cfc7
Sergei <kozelkos@yandex.ru>
2018-11-05 19:03:27 +0300

    Add lobby selection fragment with recyclerview and network code for getting list of lobbies

--- app/src/main/java/ru/ifmo/setgame/GameActivity.kt
+++ app/src/main/java/ru/ifmo/setgame/GameActivity.kt
@@ -1,17 +1,73 @@
 package ru.ifmo.setgame
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.net.Socket
+
+import kotlinx.coroutines.*
+import java.io.*
+import java.lang.Exception
 
 interface GameInterface {
+    val LOBBIES_LIST_BROADCAST: String
+        get() = "ru.ifmo.setgame.LOBBIES_LIST"
+
     fun startGame()
-    fun showScore(score : Int)
+    fun showScore(score: Int)
 }
+
+class Lobby(
+    val lobby_id: Int,
+    val max_players: Int,
+    val game_id: Int?,
+    val in_lobby: Array<Int>
+)
 
 const val SCORE_FRAGMENT_TAG = "SCORE_FRAGMENT_TAG"
 
 class GameActivity : AppCompatActivity(), GameInterface {
-    override fun showScore(score : Int) {
+    private suspend fun connect() {
+        try {
+            // init everything we need
+            val socket = Socket("18.222.225.249", 3691)
+            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val writer = BufferedWriter(OutputStreamWriter(socket.getOutputStream(), "UTF-8"))
+            val mapper = ObjectMapper().registerKotlinModule()
+
+            // send default message
+            val str = "{\"status\":\"NEW\"}"
+            writer.write(str)
+            writer.flush()
+
+            // we get player id and list of lobbies
+            val ans = reader.readLine()
+            val answer = mapper.readTree(ans)
+            val lobbies = answer.get("lobbies")
+
+            // send string because it is easier than sending array of objects
+            val lobbiesStr = mapper.writeValueAsString(lobbies)
+            Log.d("TG",lobbiesStr)
+            LocalBroadcastManager.getInstance(this).sendBroadcast(Intent(LOBBIES_LIST_BROADCAST).apply { putExtra("lobbies", lobbiesStr) })
+
+            socket.close()
+        } catch (ex: Exception) {
+            Log.e("TG", ex.toString())
+        }
+    }
+
+    override fun showScore(score: Int) {
         supportFragmentManager.beginTransaction().apply { replace(R.id.game_fragment, GameScoreFragment.newInstance(score), SCORE_FRAGMENT_TAG); commit() }
     }
 
@@ -24,5 +80,9 @@ class GameActivity : AppCompatActivity(), GameInterface {
         setContentView(R.layout.activity_game)
 
         supportFragmentManager.beginTransaction().apply { replace(R.id.game_fragment, LobbySelectionFragment()); commit() }
+
+        GlobalScope.launch {
+            connect()
+        }
     }
 }
--- app/src/main/java/ru/ifmo/setgame/LobbySelectionFragment.kt
+++ app/src/main/java/ru/ifmo/setgame/LobbySelectionFragment.kt
@@ -1,23 +1,34 @@
 package ru.ifmo.setgame
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.LocalBroadcastManager
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import kotlinx.android.synthetic.main.fragment_lobby_selection.*
 import kotlinx.android.synthetic.main.fragment_lobby_selection.view.*
 import kotlinx.android.synthetic.main.item_lobby.view.*
 import java.text.FieldPosition
 
 class LobbySelectionFragment : Fragment() {
 
-    class LobbyAdapter(var data: List<Int>) : RecyclerView.Adapter<LobbyAdapter.VH>() {
+    class LobbyAdapter(var data: Array<Lobby>) : RecyclerView.Adapter<LobbyAdapter.VH>() {
 
         class VH(tv: View) : RecyclerView.ViewHolder(tv) {
-            //val name = tv.lobby_name!!
+            val name = tv.lobby_name!!
             val nOfPlayers = tv.lobby_n_players!!
         }
 
@@ -29,7 +40,8 @@ class LobbySelectionFragment : Fragment() {
         override fun getItemCount(): Int = data.size
 
         override fun onBindViewHolder(holder: VH, position: Int) {
-            holder.nOfPlayers.text = "${data[position]} / 10"
+            holder.nOfPlayers.text = "${data[position].in_lobby.size} / ${data[position].max_players}"
+            holder.name.text = data[position].lobby_id.toString()
         }
     }
 
@@ -38,10 +50,20 @@ class LobbySelectionFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_lobby_selection, container, false)
 
-        adapter = LobbyAdapter(listOf(1, 2, 5, 2, 2, 3))
+        adapter = LobbyAdapter(arrayOf())
 
         view.recycler_view_lobbies.adapter = adapter
         view.recycler_view_lobbies.layoutManager = LinearLayoutManager(activity)
+        view.recycler_view_lobbies.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+
+        LocalBroadcastManager.getInstance(context!!).registerReceiver(object :BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val str = intent?.extras?.getString("lobbies")!!
+                val lobbies = jacksonObjectMapper().readValue<List<Lobby>>(str)
+                adapter.data = lobbies.toTypedArray()
+                adapter.notifyDataSetChanged()
+            }
+        }, IntentFilter("ru.ifmo.setgame.LOBBIES_LIST"))
         return view
     }
 }
