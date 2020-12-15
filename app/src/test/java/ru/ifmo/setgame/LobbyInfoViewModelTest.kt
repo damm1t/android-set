0d61ce463b9c10683e69bf3b84d030760ef07c5f
merak1t <donatsokolov99@gmail.com>
2020-12-15 08:28:33 +0300

    add failed lobby test

---
+++ app/src/test/java/ru/ifmo/setgame/LobbyInfoViewModelTest.kt
@@ -0,0 +1,38 @@
+package ru.ifmo.setgame
+
+import androidx.lifecycle.MutableLiveData
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+import ru.ifmo.setgame.lobby.LobbyInfoViewModel
+import ru.ifmo.setgame.network.Connector
+
+
+@RunWith(RobolectricTestRunner::class)
+class LobbyInfoViewModelTest {
+    private val liveData = MutableLiveData<String>()
+
+    private val connector = Mockito.mock(Connector::class.java).also {
+        Mockito.`when`(it.getLobbiesListLiveData()).thenReturn(liveData)
+    }
+
+    private val navigation = Mockito.mock(GameNavigation::class.java)
+
+    private val viewModel = LobbyInfoViewModel(connector, jacksonObjectMapper(), navigation)
+
+    @Test
+    fun `lobby data updates`() {
+        val lobby = Lobby(98, 2, null, arrayOf())
+        val sentLobby = "[{\"lobby_id\":98,\"max_players\":2,\"in_lobby\":[],\"game_id\":null}]"
+
+        liveData.value = sentLobby
+
+        val receivedLobby: Array<Lobby>? = viewModel.lobbiesListLiveData.value
+        Assert.assertNotNull(receivedLobby)
+        Assert.assertTrue(receivedLobby!!.isNotEmpty())
+        Assert.assertEquals(lobby, receivedLobby[0])
+    }
+}
