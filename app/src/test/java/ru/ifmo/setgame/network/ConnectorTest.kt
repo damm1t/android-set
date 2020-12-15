932a0f4aa9d5785b258d8a715c3fec12c70f8065
merak1t <donatsokolov99@gmail.com>
2020-12-15 07:10:24 +0300

    add connector tests

--- app/src/test/java/ru/ifmo/setgame/network/ConnectorTest.kt
+++ app/src/test/java/ru/ifmo/setgame/network/ConnectorTest.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.`when`
@@ -30,15 +31,43 @@ class ConnectorTest {
 
     private val connector = Connector(socket, testScope)
 
+
+    @Before
+    fun before() {
+    }
+
     @Test
-    fun foo() {
+    fun connectTest() {
+        testScope.runBlockingTest {
+            connector.connect()
+        }
+
+        val request = String(outputStream.toByteArray())
+        assertTrue(request == "{\"status\": \"NEW\"}")
+    }
+
+    @Test
+    fun leaveLobbyTest() {
         testScope.runBlockingTest {
             connector.connect()
             connector.leaveLobby()
         }
 
         val request = String(outputStream.toByteArray())
-
         assertTrue(request.contains("leave_lobby"))
     }
+
+    @Test
+    fun createLobbyTest() {
+        testScope.runBlockingTest {
+            connector.connect()
+            connector.createLobby(2)
+        }
+
+        val request = String(outputStream.toByteArray())
+        assertTrue(request.contains("\"action\": \"create_lobby\""))
+        assertTrue(request.contains("\"max_players\": 2"))
+    }
+
+
 }
